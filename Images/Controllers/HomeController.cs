794e804c450301006227c8c596024f5f521b7ea6
LeeBate <harukaxd02@gmail.com>
2022-01-05 22:41:43 +0700

    ซื้อพอประทัง

--- Images/Controllers/HomeController.cs
+++ Images/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,9 +9,10 @@ namespace Images.Controllers
 {
     public class HomeController : Controller
     {
+        private Entities db = new Entities();
         public ActionResult Index()
         {
-            return View();
+            return View(db.Pictures.ToList());
         }
 
         public ActionResult About()
@@ -25,6 +27,19 @@ namespace Images.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Picture picture = db.Pictures.Find(id);
+            if (picture == null)
+            {
+                return HttpNotFound();
+            }
+            return View(picture);
         }
     }
 }
